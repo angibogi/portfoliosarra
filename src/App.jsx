4a6c11935df72fcc2aa95b30fe40a9146cd34175
Angelo <a.pisano13@campus.unimib.it>
2025-04-28 05:21:22 +0200

    ora va

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { Makai } from "./Pages/Makai";
+import { Makai } from "./pages/Makai";
 import { Valentino } from "./pages/Valentino";
 import { Cortilia } from "./pages/Cortilia";
 import { CortiliaPage } from "./pages/CortiliaPage";

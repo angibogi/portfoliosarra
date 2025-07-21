5ef1b9454aa0b02e6c0580e635d627cfb9688f1b
Angelo <a.pisano13@campus.unimib.it>
2025-07-21 11:56:29 +0200

    email

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,4 +1,22 @@
 import React from 'react'
+import emailjs from "@emailjs/browser";
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_z6vcv9k', 'template_n1qzfhq', form.current, {
+        publicKey: 'OjOAopR2pwrEw3n97',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
 
 const contact = () => {
   return (
@@ -7,7 +25,7 @@ const contact = () => {
         <h1 className="text-white font-semibold md:text-[55px] text-[28px]">Let’s talk business</h1>
         <p className="text-white" >Now that you know a lot about me, <br /> let me know if you are interested to work with me.</p>
       </div>
-      <form>
+      <form ref={form} onSubmit={sendEmail}>
         <div className="mb-6">
           <label className="block text-white text-sm font-bold mb-2">
             Nome
@@ -18,7 +36,7 @@ const contact = () => {
           <label className="block text-white text-sm font-bold mb-2">
             E-mail
           </label>
-          <input className="bg-white-100 py-2 px-4 w-full" name="e-mail" />
+          <input className="bg-white-100 py-2 px-4 w-full" name="email" />
         </div>
         <div className="mb-10">
           <label className="block text-white text-sm font-bold mb-2">
